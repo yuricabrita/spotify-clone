733911d8c193347975ed39601da112cc9810e7e4
yuricabrita <y.cabrita@runtime-revolution.com>
2022-02-09 11:36:34 +0000

    imageCard component refactor

--- src/ImageCard.js
+++ src/ImageCard.js
@@ -3,59 +3,17 @@ import { Card, Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 export default function ImageCard({ item, type }) {
-  function renderSwitch(type) {
-    switch (type) {
-      case "album":
-        return (
-          <Card
-            style={{
-              width: "10rem",
-              marginRight: "1rem",
-            }}
-            key={item.uri}
-          >
-            <Link to={`/albums/${item.id}`} state={{ album: item }}>
-              <Card.Img variant="top" src={item.images[0].url} />
-            </Link>
-          </Card>
-        );
-      case "playlist":
-        return (
-          <Card
-            style={{
-              width: "10rem",
-              marginRight: "1rem",
-            }}
-            key={item.uri}
-          >
-            <Link to={`/playlists/${item.id}`} state={{ playlist: item }}>
-              <Card.Img variant="top" src={item.images[0].url} />
-            </Link>
-          </Card>
-        );
-      case "newReleases":
-        return (
-          <Card
-            style={{
-              width: "10rem",
-              marginRight: "1rem",
-            }}
-            key={item.uri}
-          >
-            <Link to={`/releases/${item.id}`} state={{ release: item }}>
-              <Card.Img variant="top" src={item.images[0].url} />
-            </Link>
-          </Card>
-        );
-      default:
-        return type;
+  function setLink(type) {
+    if (type === "album") {
+      return { to: `/albums/${item.id}`, state: { album: item } };
+    } else if (type === "newReleases") {
+      return { to: `/releases/${item.id}`, state: { release: item } };
+    } else if (type === "playlist") {
+      return { to: `/playlists/${item.id}`, state: { playlist: item } };
     }
   }
-  return <Container>{renderSwitch(type)}</Container>;
-}
-
-{
-  /* return (
+  const linkParams = setLink(type);
+  return (
     <Card
       style={{
         width: "20rem",
@@ -63,14 +21,9 @@ export default function ImageCard({ item, type }) {
       }}
       key={item.uri}
     >
-      {type !== "album" ? (
+      <Link to={linkParams.to} state={linkParams.state}>
         <Card.Img variant="top" src={item.images[0].url} />
-      ) : (
-        <Link to={`/album/${item.id}`} state={{ album: item }}>
-          <Card.Img variant="top" src={item.images[0].url} />
-        </Link>
-      )}
+      </Link>
     </Card>
   );
-} */
 }
